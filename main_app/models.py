7da9477dbdc66b0d691701e9d2f3840c1ce0bb2d
Ripley Mayfield <ripleymay@Ripleys-MBP.home>
2021-11-02 16:09:06 -0700

    builds gecko model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Gecko(models.Model):
+    name = models.CharField(default="No Name", max_length=50)
+    species = models.CharField(default="Unknown", max_length=50)
+    age = models.IntegerField(default=1)
+    description = models.TextField(default="None", max_length=100)
+
+    def __str__(self):
+        return (f'{self.name} is a {self.age} year old {self.species} gecko') 
--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,14 @@
 from django.shortcuts import render
-
-class Gecko():
-  def __init__(self, name, species, age, description):
-    self.name = name
-    self.species = species
-    self.age = age
-    self.description = description
-
-geckos = [
-  Gecko('Corn Dog', 'Leopard', 8, 'Young and fiesty'),
-  Gecko('Lady', 'Crested', 16, 'Very regal, lives to be held'),
-  Gecko('Lil guy', 'Common House', 5, "He's little but can fend for himself!")
-]
+from .models import Gecko
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
 
 def geckos_index(request):
-    return render(request, 'geckos/index.html', { 'geckos': geckos })
+  geckos = Gecko.objects.all()
+  return render(request, 'geckos/index.html', { 'geckos': geckos })
